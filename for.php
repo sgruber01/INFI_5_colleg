7f499218447841a2c102564683cf3f8fa22c5dfb
sgruber01 <sgruber@tgm.ac.at>
2023-11-27 14:50:01 +0100

    Das ist mein erster commit für das git_django_projekt

---
+++ for.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8" />
+	<title>for</title>
+	<meta name="generator" content="BBEdit 14.6" />
+</head>
+<body>
+
+<?php
+/* example 1 */
+$jahre=20;
+
+for ($i = 0; $i < $jahre; $i++) {
+ 
+    echo "$i <br>"; //Formel für Zinsen
+}
+
+// ausgabe Betrag mit echo
+?>
+</body>
+</html>
---
+++ osas.py
@@ -0,0 +1,16 @@
+import csv
+
+def eb(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        sortierte_wörter = sorted([row[0] for row in reader])
+        previous_letter = None
+        for word in sortierte_wörter:
+            letter = word[0]
+            if previous_letter and letter == previous_letter:
+                continue
+            print(letter, word)
+            previous_letter = letter
+
+# Test
+eb('Begriffe_Informatik_Amamsiugwudi_3BHWII.csv')
